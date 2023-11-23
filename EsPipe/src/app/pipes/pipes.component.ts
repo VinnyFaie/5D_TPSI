76b55cdc492e1e0aba7eae97174c43475b180d79
VinnyFaie <vincenzofaiella2005@gmail.com>
2023-11-23 10:54:45 +0100

    Iniziato l'esercizio 'EsPipe'

--- EsPipe/src/app/pipes/pipes.component.ts
+++ EsPipe/src/app/pipes/pipes.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./pipes.component.css']
 })
 export class PipesComponent {
+  pizza:string = "pizza quattro stagioni";
+  pasta:string = "PASTA ALLA CARBONARA";
+  valuta:number = 1235.51;
+  piGreco:number = 3.14159265359;
+  percent:number = 0.15
+  vectPipe = ["Fiat","Ford","Renault","BMW","Mercedes","Dacia"];
+  today:Date = new Date();
+
 
 }
